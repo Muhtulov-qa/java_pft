9dfa546a68dd494f2c8689293b940e84483b2e60
Muhtulov-qa <Muhtulov87@yandex.ru>
2025-06-23 14:14:09 +0400

    Добавлен ввывод нового текстового сообщения!!!

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -2,7 +2,8 @@ public class MyFirstProgram{
 
 public static void main(String[] args) {
 
-	System.out.println("Hello, World");	
+	System.out.println("Hello, World");
+	System.out.println("Cool!!!");	
 
 	}
 
